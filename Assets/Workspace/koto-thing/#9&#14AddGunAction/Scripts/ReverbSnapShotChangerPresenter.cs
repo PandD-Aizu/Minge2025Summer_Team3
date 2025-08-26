0282a46e46c71d71e6cc5b1aeae42e4bf7fda2ed
koto-thing <gotoukenta62@gmail.com>
2025-08-26 14:55:47 +0900

    FMODのSnapShotに合わせて、リバーブの種類などを変えた

---
+++ Assets/Workspace/koto-thing/#9&#14AddGunAction/Scripts/ReverbSnapShotChangerPresenter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+namespace Acoustics
+{
+    public class ReverbSnapShotChangerPresenter : MonoBehaviour, IDisposable
+    {
+        [SerializeField] private ReverbSnapShotChangerModel model;
+        [SerializeField] private ReverbSnapShotChangerView view;
+        
+        private void Start()
+        {
+            
+        }
+
+        private void Update()
+        {
+            model.CheckEnvironment();
+            model.ChangeSnapShot();
+            model.DebugRayCast();
+        }
+
+        private void SubscribeEvents()
+        {
+            
+        }
+
+        public void OnDestroy()
+        {
+            Dispose();
+        }
+
+        public void Dispose()
+        {
+            
+        }
+    }
+}
