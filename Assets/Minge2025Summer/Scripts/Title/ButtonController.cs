f311f20134d05510669d0d21e3268193586c1bdf
koto-thing <gotoukenta62@gmail.com>
2025-09-01 01:48:59 +0900

    設定をセーブする機能を追加

---
+++ Assets/Minge2025Summer/Scripts/Title/ButtonController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.SceneManagement;
+
+namespace Title
+{
+    public class ButtonController : MonoBehaviour
+    {
+        [Header("STARTボタン")]
+        [SerializeField] private string gameSceneAddress;
+
+        [Header("OPTIONボタン")] 
+        [SerializeField] private GameObject optionPanel;
+        
+        public void StartGame()
+        {
+            AsyncOperationHandle handle = Addressables.LoadSceneAsync(gameSceneAddress, LoadSceneMode.Single);
+            handle.Completed += OnSceneLoaded;
+        }
+
+        public void OpenOptions()
+        {
+            
+        }
+
+        public void CloseOptions()
+        {
+            
+        }
+
+        public void QuitGame()
+        {
+            #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+            #else
+                Application.Quit();
+            #endif
+        }
+        
+        private void OnSceneLoaded(AsyncOperationHandle handle)
+        {
+            if (handle.Status == AsyncOperationStatus.Succeeded)
+                Debug.Log("Load Complete");
+            else 
+                Debug.LogError("Load Failed");
+        }
+    }
+}
