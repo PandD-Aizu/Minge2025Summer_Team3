a7731b0d75ed952535ac042814fa09fbfa8aea2d
momiji1107 <yokohirahazuki0607@gmail.com>
2025-08-31 16:10:55 +0900

    警官がやられる処理を実装しました。
    プレイヤーの攻撃がヒットすると、警官のHPを減らす関数が呼び出されます。警官のHPが０以下になると、警官をDestroyします。

--- Assets/Workspace/momiji1107/Scripts/Police_Model.cs
+++ Assets/Workspace/momiji1107/Scripts/Police_Model.cs
@@ -5,6 +5,7 @@ public class Police_Model : MonoBehaviour
     [SerializeField] float PoliceMoveSpeed = 0.01f; //警官の移動速度
     [SerializeField] GameObject player;
     public bool Battleflag = false; //プレイヤーが範囲内にいるかどうか
+    public float PoliceHP = 50f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -20,6 +21,13 @@ public class Police_Model : MonoBehaviour
             transform.LookAt(player.transform);
             transform.position += transform.forward * PoliceMoveSpeed * Time.deltaTime;
         }
+
+        //警官がやられた時の処理
+        if(PoliceHP <= 0)
+        {
+            Debug.Log("PoliceDead");
+            Destroy(this.gameObject);
+        }
     }
 
     //フラグの切り替え
@@ -31,5 +39,10 @@ public class Police_Model : MonoBehaviour
     public void OffBattleflag()
     {
         Battleflag = false;
+    }
+
+    public void Damage(float damage)
+    {
+        PoliceHP -= damage;
     }
 }
