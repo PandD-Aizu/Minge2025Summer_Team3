e4304a5c31a780ee374fc554b1a099391b05b2ad
momiji1107 <yokohirahazuki0607@gmail.com>
2025-09-14 16:06:08 +0900

    Update Subliminal3DObjectDetectionArea_Model.cs

--- Assets/Workspace/momiji1107/Scripts/Subliminal3DObjectDetectionArea_Model.cs
+++ Assets/Workspace/momiji1107/Scripts/Subliminal3DObjectDetectionArea_Model.cs
@@ -3,20 +3,23 @@ using UnityEngine;
 public class Subliminal3DObjectDetectionArea_Model : MonoBehaviour
 {
     [SerializeField] private GameObject this3DObject; //対応するオブジェクト
-    public float DisplayTime = 3.0f;
+    public float DisplayTime = 3.0f; //表示する時間
 
+    //非表示にする
     public void HideThisObject()
     {
         Debug.Log("HideObject");
         this3DObject.gameObject.SetActive(false);
     }
 
+    //表示する
     public void AppearThisObject()
     {
         Debug.Log("appearObject");
         this3DObject.gameObject.SetActive(true);
     }
 
+    //Deleteする
     public void DestroyThisObject()
     {
         Debug.Log("destroyObject");
