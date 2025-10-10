182fa7782e8ea49d4f1b763fb5c9d31b04f8bb4e
koto-thing <gotoukenta62@gmail.com>
2025-10-11 05:34:11 +0900

    イントロ部分の作成 シーン遷移まだ

---
+++ Assets/Minge2025Summer/Scripts/Intro/IntroController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using TMPro;
+using UniRx;
+using UnityEngine;
+
+namespace Minge2025Summer.Main.Intro
+{
+    public class IntroController : MonoBehaviour
+    {
+        [Header("イントロを表示させるメインのテキスト")]
+        [SerializeField] private TextMeshProUGUI introText;
+
+        [Header("イントロ情報が入ったJSONファイルのアドレス")] 
+        [SerializeField] private string jsonAddress;
+
+        private void Start()
+        {
+            IntroTextJSONParser parser = new IntroTextJSONParser(); 
+            parser.ParseAsync(jsonAddress, entries =>
+            {
+                StartCoroutine(ShowIntroSequence(entries));
+            });
+        }
+
+        /// <summary>
+        /// イントロシーケンスを表示するコルーチン
+        /// </summary>
+        /// <param name="entries">表示するエントリ</param>
+        /// <returns></returns>
+        private IEnumerator ShowIntroSequence(List<IntroTextJSONParser.IntroTextEntry> entries)
+        {
+            introText.alpha = 0.0f;
+            foreach (var entry in entries)
+            {
+                introText.text = entry.text;
+                yield return introText.DOFade(1.0f, 0.5f).WaitForCompletion();
+                yield return new WaitForSeconds(entry.time);
+                yield return introText.DOFade(0.0f, 0.5f).WaitForCompletion();
+            }
+        }
+    }
+}
---
+++ Assets/Minge2025Summer/Scripts/Intro/IntroTextJSONParser.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+namespace Minge2025Summer.Main.Intro
+{
+    public class IntroTextJSONParser
+    {
+        [Serializable]
+        public class IntroTextEntry
+        {
+            public float time;
+            public string text;
+        }
+
+        [Serializable]
+        public class IntroTextData
+        {
+            public IntroTextEntry[] entries;
+        }
+
+        /// <summary>
+        /// JSONファイルを非同期で読み込み、パースする
+        /// </summary>
+        /// <param name="jsonAddress">読み込むJSONのアドレス</param>
+        /// <param name="onComplete"></param>
+        public void ParseAsync(string jsonAddress, Action<List<IntroTextEntry>> onComplete)
+        {
+            Addressables.LoadAssetAsync<TextAsset>(jsonAddress).Completed += op =>
+            {
+                if (op.Status == AsyncOperationStatus.Succeeded)
+                {
+                    var data = JsonUtility.FromJson<IntroTextData>(op.Result.text);
+                    onComplete?.Invoke(new List<IntroTextEntry>(data.entries));
+                }
+                else
+                {
+                    Debug.LogError("JSONロード失敗");
+                    onComplete?.Invoke(new List<IntroTextEntry>());
+                }
+            };
+        }
+    }
+}
