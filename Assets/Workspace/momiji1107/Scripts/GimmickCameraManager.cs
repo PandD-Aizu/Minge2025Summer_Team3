0599118333547146e05efce73eba7d8980797fba
momiji1107 <yokohirahazuki0607@gmail.com>
2025-12-26 14:49:29 +0900

    カメラが瞬時に切り替わるようにした。
    GimmickについているGimmickCameraManagerのcameraDistanceを変更することでギミックごとにカメラの距離を設定できます。

--- Assets/Workspace/momiji1107/Scripts/GimmickCameraManager.cs
+++ Assets/Workspace/momiji1107/Scripts/GimmickCameraManager.cs
@@ -6,11 +6,13 @@ public class GimmickCameraManager : MonoBehaviour
     [SerializeField] private GameObject fpsCamera;
     [SerializeField] private GameObject thisGimmickCamera; //このギミック用のカメラ
     [SerializeField] private PlayerGimmickController gimmickController;
+    [SerializeField, Tooltip("ギミックからのカメラの距離")] private float cameraDistance;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         thisGimmickCamera.SetActive(false);
+        thisGimmickCamera.GetComponent<CinemachinePositionComposer>().CameraDistance = cameraDistance;
     }
 
     //gimmickCameraに切り替える
--- Assets/Workspace/momiji1107/Scripts/PlayerGimmickController.cs
+++ Assets/Workspace/momiji1107/Scripts/PlayerGimmickController.cs
@@ -4,30 +4,26 @@ public class PlayerGimmickController : MonoBehaviour
 {
     [Header("インタラクト可能な最大距離")]
     [SerializeField] private float maxDistance;
-
-    [Space(20)]
-    [SerializeField] private GimmickCameraManager camManager;
-    private bool onGimmick;
-    public bool OnGimmick
-    {
-        get { return onGimmick; }
-        set { onGimmick = value; }
-    }
+    
+    private GimmickCameraManager camManager; //接触したギミックについているGimmickCameraManagerコンポーネント
+    private bool onGimmick;　//ギミック中かどうか
+    public bool OnGimmick { get { return onGimmick; } set { onGimmick = value; } }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         OnGimmick = false;
-        //camManager = GetComponent<GimmickCameraManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Eキーでギミック開始
         if (Input.GetKeyDown(KeyCode.E) && !OnGimmick)
         {
             ContactGimmick();
         }
 
+        //（テスト用、後で消す）Tキーでギミック終了
         if (Input.GetKeyDown(KeyCode.T) && OnGimmick)
         {
             camManager.ChangeCameraToMain();
@@ -43,8 +39,8 @@ public class PlayerGimmickController : MonoBehaviour
         //光線を飛ばしてギミックに当たっていたら、カメラを切り替える
         if (hit.collider != null && hit.collider.name == "Gimmick")
         {
-            //camManager = hit.collider.gameObject.GetComponent<GimmickCameraManager>();
-            //if(camManager == null) Debug.Log("can't find gimmick camera manager");
+            camManager = hit.collider.gameObject.GetComponentInChildren<GimmickCameraManager>();
+            if(camManager == null) Debug.Log("can't find gimmick camera manager");
             camManager.ChangeCameraToGimmick();
         }
     }
